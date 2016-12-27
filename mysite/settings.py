f61b7fc1045a506967b241b823aa75b8da0840c7
xiuxiu <ytxiuxiu@gmail.com>
2016-12-27 16:14:52 +1100

    added database support

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,15 +76,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': 'root'
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mysite',
+            'HOST': '127.0.0.1',
+            'USER': 'root',
+            'PASSWORD': 'root'
+        }
+    }
 
 
 # Password validation
