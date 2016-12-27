de816c641f877d668f405aadaebcb505f654adaf
xiuxiu <ytxiuxiu@gmail.com>
2016-12-27 16:42:59 +1100

    fixed bug with static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^$', include('index.urls')),
+    url(r'^', include('index.urls')),
     url(r'^travel/', include('travel.urls')),
     url(r'^admin/', admin.site.urls),
 ]
