8c8d03ad53ce82811a149fe0afdc184c194ddf9a
xiuxiu <ytxiuxiu@gmail.com>
2017-02-27 17:34:31 +1100

    add page detail

--- home/views.py
+++ home/views.py
@@ -27,28 +27,40 @@ def page(request, category, page):
     'nav_links': Link.objects.all(),
   })
 
-def category(request, category):
-  try:
-    category = Category.objects.get(link = category)
-  except (KeyError, Category.DoesNotExist):
-    page = get_object_or_404(Page, link = category)
+def category(request, category, page = None):
+  # categories / top-level pages
+  if page is None:
+    try:
+      category = Category.objects.get(link = category)
+    except (KeyError, Category.DoesNotExist):
+      page = get_object_or_404(Page, link = category)
+      return render(request, 'home/page.html', {
+        'page': page,
+        'nav_links': Link.objects.all(),
+      })
+
+    if not request.is_ajax():
+      # category page
+      return render(request, 'home/category.html', {
+        'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
+        'nav_links': Link.objects.all(),
+        'category': category,
+      })
+    else:
+      # ajax pages in category
+      pages = Page.objects.filter(category = category)
+      return HttpResponse(ExtJsonSerializer().serialize(pages, 
+        fields = ['name', 'link', 'category.link', 'image_thumbnail', 'image_size']))
+
+  # pages
+  else:
+    page = get_object_or_404(Page, link = page)
     return render(request, 'home/page.html', {
       'page': page,
       'nav_links': Link.objects.all(),
     })
   
-  if not request.is_ajax():
-    # category page
-    return render(request, 'home/category.html', {
-      'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
-      'nav_links': Link.objects.all(),
-      'category': category,
-    })
-  else:
-    # ajax pages in category
-    pages = Page.objects.filter(category = category)
-    return HttpResponse(ExtJsonSerializer().serialize(pages, 
-      fields = ['name', 'link', 'category.link', 'image_thumbnail', 'image_size']))
+  
 
 
 
