607d3ef091a688926bc15267666e8fb736e237a2
xiuxiu <ytxiuxiu@gmail.com>
2017-03-01 16:34:30 +1100

    fixed backword json bug

--- home/urls.py
+++ home/urls.py
@@ -9,6 +9,8 @@ app_name = 'home'
 urlpatterns = [
   # ex: /
   url(r'^$', views.get_index, name = 'index'),
+  url(r'^photos/$', views.get_index_photos, name = 'index-photos'),
   url(r'^(?P<category>[\w-]+)/$', views.get_category, name = 'category'),
+  url(r'^(?P<category>[\w-]+)/pages/$', views.get_category_pages, name = 'category-pages'),
   url(r'^(?P<category>[\w-]+)/(?P<page>[\w-]+)/$', views.get_category, name = 'page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -9,15 +9,15 @@ from travel.models import Photo
 
 
 def get_index(request):
-  if not request.is_ajax():
-    return render(request, 'home/index.html', {
-      'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
-      'nav_links': Link.objects.all(),
-    })
-  else:
-    photos = Photo.objects.order_by('-added_at')[:10]
-    return HttpResponse(ExtJsonSerializer().serialize(photos, 
-      fields = ['name', 'image_thumbnail', 'image_size']))
+  return render(request, 'home/index.html', {
+    'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
+    'nav_links': Link.objects.all(),
+  })
+
+def get_index_photos(request):
+  photos = Photo.objects.order_by('-added_at')[:10]
+  return HttpResponse(ExtJsonSerializer().serialize(photos, 
+    fields = ['name', 'image_thumbnail', 'image_size']))
 
 def get_page(request, page):
   page = get_object_or_404(Page, link = page)
@@ -33,19 +33,11 @@ def get_category(request, category, page = None):
   if page is None:
     try:
       category = Category.objects.get(link = category)
-
-      if not request.is_ajax():
-        # category page
-        return render(request, 'home/category.html', {
-          'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
-          'nav_links': Link.objects.all(),
-          'category': category,
-        })
-      else:
-        # ajax pages in category
-        pages = Page.objects.filter(category = category)
-        return HttpResponse(ExtJsonSerializer().serialize(pages, 
-          fields = ['name', 'link', 'category.link', 'image_thumbnail', 'image_size']))
+      return render(request, 'home/category.html', {
+        'ground_floor_timber': GroundFloorTimber.objects.order_by('?').first(),
+        'nav_links': Link.objects.all(),
+        'category': category,
+      })
 
     except (KeyError, Category.DoesNotExist):
       return get_page(request, category)
@@ -54,7 +46,11 @@ def get_category(request, category, page = None):
   else:
     return get_page(request, page)
   
-  
+def get_category_pages(request, category):
+  category = Category.objects.get(link = category)
+  pages = Page.objects.filter(category = category)
+  return HttpResponse(ExtJsonSerializer().serialize(pages, 
+    fields = ['name', 'link', 'category.link', 'image_thumbnail', 'image_size']))
 
 
 
